30c4207f3806b2944de432fa6e0a21d85a6f6fec
Ryan Strong <ryanstrong2@yahoo.com>
2017-03-27 18:15:58 -0500

    user name on post

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ class BlogHandler(webapp2.RequestHandler):
             Get all posts by a specific user, ordered by creation date (descending).
             The user parameter will be a User object.
         """
-        userposts = Post.user().order('-created')
+        userposts = Post.all().filter("author", user).order('-created')
         # TODO - filter the query so that only posts by the given user
-        return userposts.fetch(user=user, limit=limit, offset=offset)
-
+        return userposts.fetch(limit, offset)
+        # return query.fetch(limit, offset)
     def get_user_by_name(self, username):
         """ Get a user object from the db, based on their username """
         user = db.GqlQuery("SELECT * FROM User WHERE username = '%s'" % username)
@@ -70,13 +70,12 @@ class IndexHandler(BlogHandler):
         self.response.write(response)
 
 class BlogIndexHandler(BlogHandler):
-
     # number of blog posts per page to display
     page_size = 5
-
     def get(self, username=""):
         """ """
-
+        # username= self.get_user_by_name(username)
+        # author = self.get_user_by_name(author)
         # If request is for a specific page, set page number and offset accordingly
         page = self.request.get("page")
         offset = 0
@@ -85,7 +84,6 @@ class BlogIndexHandler(BlogHandler):
             offset = (int(page) - 1) * self.page_size
         else:
             page = 1
-
         # Fetch posts for all users, or a specific user, depending on request parameters
         if username:
             user = self.get_user_by_name(username)
@@ -132,17 +130,16 @@ class NewPostHandler(BlogHandler):
         body = self.request.get("body")
 
         if title and body:
-
-            # create a new Post object and store it in the database
             post = Post(
-                title=title,
-                body=body,
+                # parent = blogkey(), #make orginizing easier datastore
+                title= title,
+                body= body,
                 author=self.user)
+                # author= author) works
+                # username = username)#self.user
             post.put()
 
-            # get the id of the new post, so we can render the post's page (via the permalink)
-            id = post.key().id()
-            self.redirect("/blog/%s" % id)
+            self.redirect("/blog/%s" % post.key().id())
         else:
             error = "we need both a title and a body!"
             self.render_form(title, body, error)
@@ -151,16 +148,17 @@ class ViewPostHandler(BlogHandler):
 
     def get(self, id):
         """ Render a page with post determined by the id (via the URL/permalink) """
-
+        # author =self.request.get("author")
+        author = User.get_by_id(int(id))# User.get_by_id(int(id))
         post = Post.get_by_id(int(id))
         if post:
             t = jinja_env.get_template("post.html")
             response = t.render(post=post)
+####            # response = t.render(post=post, author=author)
         else:
             error = "there is no post with id %s" % id
             t = jinja_env.get_template("404.html")
             response = t.render(error=error)
-
         self.response.out.write(response)
 
 class SignupHandler(BlogHandler):
@@ -184,12 +182,11 @@ class SignupHandler(BlogHandler):
             return verify
 
     def validate_email(self, email):
-
         # allow empty email field
         if not email:
             return ""
 
-        EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+        EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
         if EMAIL_RE.match(email):
             return email
 
@@ -236,16 +233,12 @@ class SignupHandler(BlogHandler):
             self.login_user(user)
         else:
             has_error = True
-
             if not username:
                 errors['username_error'] = "That's not a valid username"
-
             if not password:
                 errors['password_error'] = "That's not a valid password"
-
             if not verify:
                 errors['verify_error'] = "Passwords don't match"
-
             if email is None:
                 errors['email_error'] = "That's not a valid email"
 
@@ -257,7 +250,6 @@ class SignupHandler(BlogHandler):
             self.redirect('/blog/newpost')
 
 class LoginHandler(BlogHandler):
-
     # TODO - The login code here is mostly set up for you, but there isn't a template to log in
 
     def render_login_form(self, error=""):
@@ -277,17 +269,25 @@ class LoginHandler(BlogHandler):
 
         if not user:
             self.render_login_form(error="Invalid username")
+        # elif not submitted_password:
+        #     self.render_login_form(error="Invalid password")
         elif hashutils.valid_pw(submitted_username, submitted_password, user.pw_hash):
             self.login_user(user)
             self.redirect('/blog/newpost')
         else:
-            self.render_login_form(error="Invalid password")
+            self.render_login_form(error = "error Invalid password")
+
+
 
 class LogoutHandler(BlogHandler):
 
     def get(self):
         self.logout_user()
         self.redirect('/blog')
+
+class ErrorHandler(BlogHandler):
+    def get(self):
+        self.response.out.write('404.html')
 
 app = webapp2.WSGIApplication([
     ('/', IndexHandler),
@@ -297,7 +297,8 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/blog/<username:[a-zA-Z0-9_-]{3,20}>', BlogIndexHandler),
     ('/signup', SignupHandler),
     ('/login', LoginHandler),
-    ('/logout', LogoutHandler)
+    ('/logout', LogoutHandler),
+    ('/404', ErrorHandler)
 ], debug=True)
 
 # A list of paths that a user must be logged in to access
